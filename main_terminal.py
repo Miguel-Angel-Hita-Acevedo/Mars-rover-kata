db02fcf44bfb6cd2d3265e9d49baa439bea91ae6
MiguelAngelHitaAcevedo <miguelangelhitaacev@gmail.com>
2023-06-14 19:59:52 +0200

    baby Steps: making firsts test and pass it

--- main_terminal.py
+++ main_terminal.py
@@ -1,3 +1,27 @@
 
-def __init__(self, comands):
-    pass
+class MainTerminal():
+    def __init__(self):
+        pass
+
+    def execute_command(self, input):
+        x = 0
+        y = 0
+        rotation = 'N'
+        if input == "":
+            return "0:0:N"
+        # "MRMM" = 1:2:E
+        for command in input:
+            if command == "M":
+                if rotation == "N":
+                    y += 1
+                if rotation == "E":
+                    x += 1
+            if command == "R" and rotation == "N":
+                rotation = "E"
+                
+
+
+        return self._buildResult(x, y, rotation)
+
+    def _buildResult(self,x, y, rotation):
+            return str(x) + ":" + str(y) + ":" + rotation
--- test/main_terminal_test.py
+++ test/main_terminal_test.py
@@ -1,7 +1,19 @@
 import unittest
+from main_terminal import MainTerminal
 
 
 class main_terminal_test(unittest.TestCase):
     def test_empty_parameters_then_dont_move(self):
-        
-        pass
+        main_terminal = MainTerminal()
+        output = main_terminal.execute_command("")
+        self.assertEqual("0:0:N", output)
+
+    def test_one_move(self):
+        main_terminal = MainTerminal()
+        output = main_terminal.execute_command("M")
+        self.assertEqual("0:1:N", output)
+
+    def test_one_move_turn_right_and_two_move(self):
+        main_terminal = MainTerminal()
+        output = main_terminal.execute_command("MRMM")
+        self.assertEqual("2:1:E", output)
